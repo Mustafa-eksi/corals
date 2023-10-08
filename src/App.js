b17e5312ac72b8f9b9902cedc0d7b226f72df2a2
Mustafa Ekşi <mustafa@localhost.localdomain>
2023-10-08 16:16:26 +0300

    Added core features like data virtualizations on map and displaying information about them.

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 import "./App.css"
 
-import map from './nasa_blue_marbel_waters_resized.jpg';
-import clorophyl from './deneme_data_aph_443.png';
 import ButtonsOverlay from "./ButtonsOverlay";
-import DataOverlay from "./DataOverlay";
-import GeoTIFF, {fromBlob, fromFile} from 'geotiff';
-import { Map, TileLayer, ImageOverlay } from 'react-leaflet';
-
 import MainMap from './nasa_blue_marbel_waters_resized.jpg';
+import { Button, Accordion } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Maps_data = require.context('./data_images', true);
 
 const imageList = Maps_data.keys().map(image => Maps_data(image));
+
+const yazilar = [{title: "Changes in chlorophyll concentrations", content: "In the ocean, high chlorophyll concentrations are concentrated in areas where tiny, plant-like organisms (phytoplankton) thrive. The highest chlorophyll concentrations are in cold polar waters or in places where ocean currents bring cold water to the surface, such as around the equator and along the shores of continents. It is not the cold water itself that stimulates the phytoplankton. Instead, the cool temperatures are often a sign that the water has welled up to the surface from deeper in the ocean, carrying nutrients that have built up over time. In polar waters, nutrients accumulate in surface waters during the dark winter months when plants can’t grow. When sunlight returns in the spring and summer, the plants flourish in high concentrations.\nMeasurements of global chlorophyll and vegetation are valuable to scientists because they provide insight into the carbon cycle. Scientists use ocean chlorophyll and vegetation measurements to determine the planet’s net primary productivity: how much carbon is being used by the plants to grow. Carbon cycles through the oceans, soil and rocks, plants on land and in the ocean, and atmosphere. The build up of carbon dioxide released into the atmosphere by burning fossil fuel is the primary cause of global warming. The global biosphere has been helping to offset some of the excess carbon dioxide people have been pumping into the atmosphere.", read_more:["https://earthobservatory.nasa.gov/world-of-change/Biosphere#:~:text=Ocean%20chlorophyll%20concentrations%20can%20change,are%20grazing%20in%20an%20area"]}, 
+{title: "Particulate Organic Carbon (POC)", content: "This algorithm provides an estimate of the concentration of particulate organic carbon (POC) in milligrams per cubic meter (mg m-3). It calculates this estimate based on an empirical relationship that has been developed using measurements taken in the field of POC and the ratios of blue-to-green bands in remote sensing reflectance (Rrs).", read_more:["https://oceancolor.gsfc.nasa.gov/resources/atbd/poc/"]},
+{title: "Ocean has fever!", content: "Gavin Schmidt, the director of NASA's Goddard Institute for Space Studies, explains that long-term trends of increasing heat and warmer sea surface temperatures worldwide are primarily due to human activities, specifically the substantial greenhouse gas emissions since the industrial era. While short-term factors like weather, wind patterns, and aerosols can influence sea surface temperatures regionally, their impact on the global long-term mean is minimal. Research indicates that up to 93% of the excess heat resulting from greenhouse gas emissions has been absorbed by the ocean, mainly near the surface, which has significant implications for global warming and sea-level rise. Therefore, understanding the rate and significance of oceanic heat uptake is a key research focus.", read_more:["https://www.nature.com/articles/s41558-020-0822-0",
+"https://earthobservatory.nasa.gov/images/151743/the-ocean-has-a-fever"]},
+{title: "Ocean has fever!", content: "Gavin Schmidt, the director of NASA's Goddard Institute for Space Studies, explains that long-term trends of increasing heat and warmer sea surface temperatures worldwide are primarily due to human activities, specifically the substantial greenhouse gas emissions since the industrial era. While short-term factors like weather, wind patterns, and aerosols can influence sea surface temperatures regionally, their impact on the global long-term mean is minimal. Research indicates that up to 93% of the excess heat resulting from greenhouse gas emissions has been absorbed by the ocean, mainly near the surface, which has significant implications for global warming and sea-level rise. Therefore, understanding the rate and significance of oceanic heat uptake is a key research focus.", read_more:["https://www.nature.com/articles/s41558-020-0822-0",
+"https://earthobservatory.nasa.gov/images/151743/the-ocean-has-a-fever"]},
+]
 
 function imgListToMaps(list) {
   var gercek_isimler = []
@@ -51,6 +55,32 @@ function GetDateString(human_ind, date_ind) {
   return datePlusExt.substring(0, datePlusExt.length-4);
 }
 
+class OceanInfo extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Phytoplanctons' life cycle</Accordion.Header>
+        <Accordion.Body>
+          Content
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          content 2
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+      </div>
+      );
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -59,7 +89,8 @@ class App extends Component {
       open_page_ind: 0,
       map_ind: 0,
       current_date: 1,
-      current_human: 0
+      current_human: 0,
+      play: false
     };
   }
   render() {
@@ -68,29 +99,33 @@ class App extends Component {
       const { imageData, metadata } = await fromBlob(blob);
       console.log(metadata)
     });*/
-    
+    document.title = "Corals";
+
     return (
       <div>
         <div className="tools" style={{display: "flex", flexDirection:"row"}}>
-              <h1 style={{fontSize:'14pt', margin: "15px"}}>Learn Oceans!</h1>
+        <h1 style={{fontSize:'14pt', margin: "20px"}}>Corals</h1>
+        
               <div id="menu_buttons" style={{margin: "10px"}}>
-                <button id="menu1" className={"menu-button" + (this.state.current_mode === 0 ? " clicked" : "")} onClick={()=>{
-                    document.getElementById("menu2").classList.remove("clicked");
-                    document.getElementById("menu1").classList.add("clicked");
-                    this.setState({current_mode: 0, map_ind: 0})
-                  }}>
-                  Select ocean
-                </button>
-                
-                <button id="menu2" className={"menu-button" + (this.state.current_mode === 1 ? " clicked" : "")} onClick={()=>{
-                    document.getElementById("menu1").classList.remove("clicked");
-                    document.getElementById("menu2").classList.add("clicked");
-                    this.setState({map_ind: 1, current_mode: 1})
-                  }}>
-                  Impact on global warming 
-                </button>
+                  <Button variant={this.state.current_mode === 0 ? "dark" : "light"} id="menu1" onClick={()=>{
+                      document.getElementById("menu2").classList.remove("clicked");
+                      document.getElementById("menu1").classList.add("clicked");
+                      this.setState({current_mode: 0, map_ind: 0})
+                    }}>
+                    Select ocean
+                  </Button>
+                  
+                  <Button id="menu2" variant={this.state.current_mode === 1 ?  "dark" : "light"} onClick={()=>{
+                      document.getElementById("menu1").classList.remove("clicked");
+                      document.getElementById("menu2").classList.add("clicked");
+                      this.setState({map_ind: 1, current_mode: 1})
+                    }}>
+                    Impact on global warming 
+                  </Button>
               </div>
-            </div>
+
+
+        </div>
       <div style={{display: "flex", flexDirection: "row"}}>
       <div style={{width:'75%', height:'100%'}}>
       <TransformWrapper
@@ -103,7 +138,7 @@ class App extends Component {
             
             <TransformComponent style={{height:'100%', width:'100%', alignItems: "center", backgroundColor: "red"}}>
                   <img src={this.state.current_mode === 0 ? MainMap : get_current_map(this.state.current_human, parseInt(this.state.current_date))} alt="test" style={{ height: '1000px', width:'100%'}}/>
-                  {this.state.current_mode === 0 ? <ButtonsOverlay changeState={()=>{this.setState({open_page_ind: 1})}} style={{}}></ButtonsOverlay> : <div/>}
+                  {this.state.current_mode === 0 ? <ButtonsOverlay changeState={(inde)=>{this.setState({open_page_ind: inde})}} style={{}}></ButtonsOverlay> : <div/>}
             </TransformComponent>
           </React.Fragment>
         )}
@@ -113,16 +148,25 @@ class App extends Component {
         <div className="sidebar" style={{}}>
           <h1 style={{}}>{this.state.current_mode === 0 ? "Select an ocean to view information" : "Select map mode"}</h1>
           <div className="sidebar-contents">
-          {this.state.current_mode === 0 ? "bilgi":
+          {this.state.current_mode === 0 ? <OceanInfo index={this.state.open_page_ind}></OceanInfo>:
             <div className="sidebar-contents">
               <div className="slidecontainer">
-                <input type="range" min="1" max={human_names[this.state.current_human].dates.length} value={this.state.current_date} className="slider" id="myRange" onChange={(e)=>{this.setState({current_date: e.target.value})}}/>
+                <input type="range" min="1" max={human_names[this.state.current_human].dates.length} value={this.state.current_date} className="slider" id="myRange" onChange={(e)=>{this.setState({current_date: e.target.value})}} class="date-slider"/>
                 {GetDateString(this.state.current_human, this.state.current_date)}
               </div>
 
               {human_names.map((human_name, index)=>{
-                return (<button className="map_button" key={index} onClick={()=>{this.setState({map_ind: index+1, current_human: index, current_date: 1})}}>{human_name.isim}</button>);
+                return (<Button variant={this.state.current_human !== index ? "light" : "dark"} className="map_button" key={index} onClick={()=>{this.setState({map_ind: index+1, current_human: index, current_date: 1})}}>{human_name.isim}</Button>);
               })}
+              <div style={{wordWrap: "break-word"}}>
+                <h1>{yazilar[this.state.current_human].title}</h1>
+                <div>{yazilar[this.state.current_human].content}</div>
+                <div>Further read: 
+                  {yazilar[this.state.current_human].read_more.map(e=><div><a href={yazilar[this.state.current_human].read_more}>
+                    {e}
+                  </a><br/></div>)}
+                </div>
+              </div>
             </div>
           }
         </div>
--- src/ButtonsOverlay.js
+++ src/ButtonsOverlay.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from "react-bootstrap";
 
 class ButtonsOverlay extends Component {
     constructor(props) {
@@ -8,7 +10,11 @@ class ButtonsOverlay extends Component {
     render() {
         return (
             <div style={{position: "absolute", width:'100%', height:'100%'}}>
-<button id="overlay_button" style={{position: "absolute", left: '4.5%'}} onClick={()=>{this.props.changeState()}}>deneme</button>
+                <Button variant="danger" id="overlay_button" style={{position: "absolute", left: '50%', top: '5%', fontSize: '9pt'}} onClick={()=>{this.props.changeState(2)}}>Arctic Ocean</Button>
+                <Button variant="danger" id="overlay_button" style={{position: "absolute", left: '13%', top: '35%', fontSize: '9pt'}} onClick={()=>{this.props.changeState(1)}}>Vaquita dolphin</Button>
+                <Button variant="danger" id="overlay_button" style={{position: "absolute", left: '31%', top: '30%', fontSize: '9pt'}} onClick={()=>{this.props.changeState(2)}}>North Atlantic Right Whale</Button>
+                <Button variant="danger" id="overlay_button" style={{position: "absolute", left: '80%', top: '37%', fontSize: '9pt'}} onClick={()=>{this.props.changeState(2)}}>Indian Ocean</Button>
+                <Button variant="light" id="overlay_button" style={{position: "absolute", left: '47%', top: '   ', fontSize: '9pt'}} onClick={()=>{this.props.changeState(2)}}>Dugong</Button>
             </div>
         );
     }
